3e918e453b559e2f2f042d68dff95e808086f0bb
myusername <myemail>
2024-02-14 15:49:28 +0530

    background css changed

--- src/comp/land_3d/Land_3d.jsx
+++ src/comp/land_3d/Land_3d.jsx
@@ -1,12 +1,21 @@
 import Spline from '@splinetool/react-spline';
 import '../../assets/bg.css'
+import Resume_review from '../Resume_review/Resume_review';
+import TimeLine from '../Timeline/Timeline';
+import About from '../About/About';
+import Fotter from '../Fotter/Fotter';
 
 export default function Land_3d() {
   return (
-    <div className='render_3d' >
+    <div className='landingpage'>
+      <div className='render_3d' id='home-top' >
       <div id='stars'></div>
       <Spline scene="https://prod.spline.design/sqefH0tO5t7porj6/scene.splinecode" />
-
+    </div>
+    <Resume_review />
+      <TimeLine />
+      <About />
+      <Fotter />
     </div>
   );
 }
--- src/comp/navbar/Navbar.jsx
+++ src/comp/navbar/Navbar.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 import { Flex } from "antd";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="w-full">
-      <Flex justify="space-between">
-        <div className="text-white font-bold">Auto Resume</div>
+    <div className="navbar-container">
+      <div className="nav-sub">
+        <div className="text-white font-bold text-2xl">Auto Resume</div>
+        <div className="nav-items">
+          <a className="nav-item" href="#home-top">
+            Home
+          </a>
+          <a className="nav-item" href="#timeline">
+            Time Line
+          </a>
+          <a className="nav-item" href="#about">
+            About
+          </a>
+        </div>
         <div className="h-4 relative">
           <Link
-            to='/signin'
+            to="/signin"
             class="relative inline-flex items-center justify-start px-6 py-0 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group h-10"
           >
             <span class="w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
             <span class="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
-             SignIn | SignUp
+              SignIn | SignUp
             </span>
           </Link>
         </div>
-      </Flex>
+      </div>
     </div>
   );
 };
